8dac478622779b604304ce6a42255d828fa8eecf
Mrbysco <Mrbysco@users.noreply.github.com>
2022-12-03 15:22:21 +0100

    Update forge

--- src/main/java/eu/midnightdust/motschen/rocks/client/ClientHandler.java
+++ src/main/java/eu/midnightdust/motschen/rocks/client/ClientHandler.java
@@ -7,14 +7,16 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 public class ClientHandler {
 	public static void onClientSetup(final FMLClientSetupEvent event) {
-		ItemProperties.register(RocksRegistry.STARFISH_ITEM.get(),
-				new ResourceLocation("red"), (stack, world, entity, seed) ->
-						(stack.getTag() != null && stack.getTag().getString("variation").equals("red")) ? 1 : 0);
-		ItemProperties.register(RocksRegistry.STARFISH_ITEM.get(),
-				new ResourceLocation("pink"), (stack, world, entity, seed) ->
-						(stack.getTag() != null && stack.getTag().getString("variation").equals("pink")) ? 1 : 0);
-		ItemProperties.register(RocksRegistry.STARFISH_ITEM.get(),
-				new ResourceLocation("orange"), (stack, world, entity, seed) ->
-						(stack.getTag() != null && stack.getTag().getString("variation").equals("orange")) ? 1 : 0);
+		event.enqueueWork(() -> {
+			ItemProperties.register(RocksRegistry.STARFISH_ITEM.get(),
+					new ResourceLocation("red"), (stack, world, entity, seed) ->
+							(stack.getTag() != null && stack.getTag().getString("variation").equals("red")) ? 1 : 0);
+			ItemProperties.register(RocksRegistry.STARFISH_ITEM.get(),
+					new ResourceLocation("pink"), (stack, world, entity, seed) ->
+							(stack.getTag() != null && stack.getTag().getString("variation").equals("pink")) ? 1 : 0);
+			ItemProperties.register(RocksRegistry.STARFISH_ITEM.get(),
+					new ResourceLocation("orange"), (stack, world, entity, seed) ->
+							(stack.getTag() != null && stack.getTag().getString("variation").equals("orange")) ? 1 : 0);
+		});
 	}
 }

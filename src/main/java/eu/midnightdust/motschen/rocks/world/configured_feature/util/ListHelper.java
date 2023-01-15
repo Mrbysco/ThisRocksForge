04906b220260010ec7ec1c30cccaeb0481f2b55f
Mrbysco <Mrbysco@users.noreply.github.com>
2023-01-15 21:32:27 +0100

    Fix crash due to mistake

--- src/main/java/eu/midnightdust/motschen/rocks/world/configured_feature/util/ListHelper.java
+++ src/main/java/eu/midnightdust/motschen/rocks/world/configured_feature/util/ListHelper.java
@@ -2,11 +2,13 @@ package eu.midnightdust.motschen.rocks.world.configured_feature.util;
 
 import net.minecraft.world.level.levelgen.placement.PlacementModifier;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListHelper {
 	public static List<PlacementModifier> getMergedModifierList(List<PlacementModifier> defaultList, PlacementModifier... placements) {
-		List<PlacementModifier> modifiers = List.of(placements);
+		List<PlacementModifier> modifiers = new ArrayList<>();
+		modifiers.addAll(List.of(placements));
 		modifiers.addAll(defaultList);
 
 		return modifiers;

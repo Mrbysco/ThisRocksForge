a180d63376671c9a16d16e380631d1c0c53050c4
Mrbysco <Mrbysco@users.noreply.github.com>
2025-06-25 22:01:13 +0200

    Port to 1.21.6

--- src/main/java/eu/midnightdust/motschen/rocks/datagen/RocksDatagen.java
+++ src/main/java/eu/midnightdust/motschen/rocks/datagen/RocksDatagen.java
@@ -11,7 +11,7 @@ import net.neoforged.neoforge.data.event.GatherDataEvent;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
-@EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber
 public class RocksDatagen {
 	@SubscribeEvent
 	public static void gatherData(GatherDataEvent.Client event) {
